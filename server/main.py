727c5f3fb7efc5a8c2be99cbc915e6e0f6224321
Cole B Kisten <130485191+BR0K3Nreality@users.noreply.github.com>
2023-05-31 12:37:33 +0200

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,96 +1,139 @@
 import numpy as np
 from PIL import Image
+from midiutil import MIDIFile
+from flask import Flask, request, send_from_directory
+from scipy.signal import butter, lfilter
+from midi2audio import FluidSynth
+import os
+import sys
+from flask_cors import CORS
+import threading
+import numpy as np
+from PIL import Image
 from pydub import AudioSegment
 from pydub.effects import low_pass_filter
 from tqdm import tqdm
 import os
 import sys
 from flask import Flask, request, send_file
+from flasgger import Swagger, swag_from
+
 
 app = Flask(__name__)
+CORS(app)
+swagger = Swagger(app)
 
-def karplus_strong(frequency, duration, sample_rate=44100, blend=0.5):
-    N = int(sample_rate / frequency)
-    buffer = np.random.uniform(-1, 1, size=N)
-    output = np.zeros(int(sample_rate * duration))
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
 
-    for i in range(output.shape[0]):
-        output[i] = buffer[i % N]
-        buffer[i % N] = blend * (buffer[i % N] + buffer[(i - 1) % N])
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
 
-    return output
+def pixel_to_midi(pixel, time, track, channel, velocity, duration, midi):
+    red, green, blue = pixel
+    midi.addNote(track, channel, red, time, duration, velocity)
+    midi.addNote(track, channel, green, time + duration, duration, velocity)
+    midi.addNote(track, channel, blue, time + 2 * duration, duration, velocity)
 
-def pixel_to_audio_sample(pixel, sample_rate=44100):
-    red, green, blue = pixel / 3
-    base_frequency = 82.41 + (((red + green + blue) / 765) * (987.77 - 82.41))
-
-    red_frequency = base_frequency
-    green_frequency = base_frequency * (2**(6/12))  # Phrygian fifth
-    blue_frequency = base_frequency * (2**(1/12))  # Phrygian second
-
-    duration = 0.15
-
-    audio_sample_red = karplus_strong(red_frequency, duration, sample_rate)
-    audio_sample_green = karplus_strong(green_frequency, duration, sample_rate)
-    audio_sample_blue = karplus_strong(blue_frequency, duration, sample_rate)
-
-    sample_width = audio_sample_red.dtype.itemsize
-    channels = 1
-    remainder_red = len(audio_sample_red) % (sample_width * channels)
-    remainder_green = len(audio_sample_green) % (sample_width * channels)
-    remainder_blue = len(audio_sample_blue) % (sample_width * channels)
-
-    if remainder_red != 0:
-        audio_sample_red = audio_sample_red[:-remainder_red]
-    if remainder_green != 0:
-        audio_sample_green = audio_sample_green[:-remainder_green]
-    if remainder_blue != 0:
-        audio_sample_blue = audio_sample_blue[:-remainder_blue]
-
-    audio_sample = (audio_sample_red + audio_sample_green + audio_sample_blue) / 3
-
-    return audio_sample
-
-def image_to_audio(image, size=(10, 10), sample_rate=44100):
+def image_to_midi(image, size=(10, 10), track=0, channel=0, velocity=100, duration=1):
     image = image.resize(size)
-
     pixel_data = np.array(image)
 
-    audio_segments = []
-    for row in tqdm(pixel_data, desc="Processing audio segments"):
+    midi = MIDIFile(1)
+    midi.addTempo(track, 0, 120)  # Add tempo of 120 BPM to track 0 at time 0
+
+
+    time = 0
+    for row in pixel_data:
         for pixel in row:
-            audio_sample = pixel_to_audio_sample(pixel, sample_rate)
-            audio_sample = audio_sample / np.max(np.abs(audio_sample)) * 0.5  # Scale the audio to increase volume
+            pixel_to_midi(pixel, time, track, channel, velocity, duration, midi)
+            time += 3 * duration
+    return midi
 
-            audio_segment = AudioSegment(
-                audio_sample.tobytes(),
-                frame_rate=sample_rate,
-                sample_width=4,
-                channels=1,
-            )
-            audio_segments.append(audio_segment)
-
-    combined_audio = sum(audio_segments)
-    combined_audio = low_pass_filter(combined_audio, 5000)
-    return combined_audio
+def delete_file(path):
+    if os.path.exists(path):
+        os.remove(path)
+    else:
+        print("The file does not exist")
 
 def convertImg(path):
     image = Image.open(path).convert("RGB")
-    audio = image_to_audio(image)
-    output_filename = f"{path.split('/')[-1].split('.')[0]}.wav"
-    audio.export("./output/" + output_filename, format="wav")
-    print("Processing completed successfully file stored in output/" + output_filename)
+    midi = image_to_midi(image)
+    output_filename = f"{path.split('/')[-1].split('.')[0]}.mid"
+    output_filepath = os.path.join("./output/", output_filename)
+    with open(output_filepath, "wb") as output_file:
+        midi.writeFile(output_file)
+
+    # Generate audio from MIDI
+    output_audio_filename = f"{path.split('/')[-1].split('.')[0]}.wav"
+    # FluidSynth().midi_to_audio(output_filepath, "./output/" + output_audio_filename) Convert midi to wav
+    
+    print("Processing completed successfully. File stored in output/" + output_audio_filename)
     return output_filename
 
 @app.route('/convert', methods=['POST'])
+@swag_from({
+    'parameters': [
+        {
+            'name': 'file',
+            'in': 'formData',
+            'type': 'file',
+            'required': 'true'
+        }
+    ],
+    'responses': {
+        200: {
+            'description': 'Success',
+            'schema': {
+                'type': 'file',
+                'description': 'The generated .mid file'
+            }
+        }
+    },
+    'consumes':['multipart/form-data'],
+})
 def convert_image():
+    """Converts the uploaded image file to a .mid file
+    ---
+    post:
+      parameters:
+        - in: formData
+          name: file
+          type: file
+          required: true
+          description: The image file to convert
+      responses:
+        200:
+          description: The generated .mid file
+          schema:
+            type: file
+            description: The generated .mid file
+    """
     file = request.files['file']
-    file.save(file.filename)
-    output_filename = convertImg(file.filename)
-    os.remove(file.filename)
-    return send_file("./output/" + output_filename)
+    filename = file.filename  # Get the uploaded file name
+    file.save(filename)
+    output_filename = convertImg(filename)
+    os.remove(filename)
+
+    # Set the content disposition header to force download with the original image name and .mid extension
+    response = send_from_directory("./output/", output_filename, as_attachment=True)
+    response.headers["Content-Disposition"] = f"attachment; filename={filename}.mid"
+    
+    # Delete the output file after 20 minutes
+    timer = threading.Timer(1200, delete_file, ["./output/" + output_filename])
+    timer.start()
+
+    return response
 
 if __name__ == "__main__":
     if not os.path.exists("./output/"):
         os.makedirs("./output/")
-    app.run(host='0.0.0.0')
+        
+    # Default port will be 5000 if no argument is provided
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 1223
+    app.run(host='0.0.0.0', port=port)
