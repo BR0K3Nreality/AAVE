f78d3a8ccb5c252cd1ab380a62913acb85dbf626
Cole B Kisten <130485191+BR0K3Nreality@users.noreply.github.com>
2023-09-07 14:29:42 +0200

    Update main.py

--- server/main.py
+++ server/main.py
@@ -62,8 +62,12 @@ def delete_file(path):
         print("The file does not exist")
 
 def convertImg(path):
-    image = Image.open(path).convert("RGB")
-    midi = image_to_midi(image)
+image = Image.open(path).convert("RGB")
+    
+    # Compress the image
+    compressed_image_path = path.replace(".png", "_compressed.png")
+    compress_image(path, compressed_image_path)
+    
     output_filename = f"{path.split('/')[-1].split('.')[0]}.mid"
     output_filepath = os.path.join("./output/", output_filename)
     with open(output_filepath, "wb") as output_file:
